e622c137f2a4b8433e2054fd158f799225b9a2ed
Aris Culala <aris.culala@taiger.com>
2021-04-04 01:35:53 +0800

    Initial commit for template spring boot - liquibase - postgresql

---
+++ src/main/java/com/liquibase/generator/template/SpringbootLiquibaseGeneratorTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.liquibase.generator.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLiquibaseGeneratorTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLiquibaseGeneratorTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquibase/generator/template/SpringbootLiquibaseGeneratorTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibase.generator.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiquibaseGeneratorTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
